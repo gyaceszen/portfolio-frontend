89590fe9f2fdf2a241dbc35099edf445f8e60f78
Gaston Yaceszen <gaston.vy@gmail.com>
2022-04-21 00:40:00 -0300

    componentes creados. queda pasar el contenido del index a cada uno.

--- portfolioAngular/src/app/app.module.ts
+++ portfolioAngular/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './misComponentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './misComponentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './misComponentes/educacion/educacion.component';
+import { SkillsComponent } from './misComponentes/skills/skills.component';
+import { ProyectosComponent } from './misComponentes/proyectos/proyectos.component';
+import { FooterComponent } from './misComponentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
