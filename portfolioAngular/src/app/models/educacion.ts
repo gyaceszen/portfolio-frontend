c1ff3b6f1f2216a5d5fa2be06f9a469c68bce532
Gaston Yaceszen <gaston.vy@gmail.com>
2022-05-21 01:06:36 -0300

    trayendo usuario y experiencia desde bdd con backend

---
+++ portfolioAngular/src/app/models/educacion.ts
@@ -0,0 +1,7 @@
+export interface Educacion{
+    idEducacion:number;
+    nombreInstEducacion:string;
+    linkEducacion:string;
+    descEducacion:string;
+    urlImgEducacion:string;
+}
---
+++ portfolioAngular/src/app/servicios/educacion.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Educacion } from '../models/educacion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EducacionService {
+
+  private apiServerUrl=environment.apiBaseUrl;
+
+  constructor(private http:HttpClient) { }
+
+  public getEducacion():Observable<Educacion[]>{
+    return this.http.get<Educacion[]>(`${this.apiServerUrl}/educacion/todas`);
+  }
+  
+  public agregarEducacion(idEducacion: number):Observable<Educacion>{
+    return this.http.post<Educacion>(`${this.apiServerUrl}/educacion/nueva`, idEducacion);
+  }
+
+  public editarEducacion(educacion: Educacion):Observable<Educacion>{
+    return this.http.put<Educacion>(`${this.apiServerUrl}/educacion/editar`, educacion);
+  }
+
+  public eliminarEducacion(idEducacion: number):Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/educacion/borrar/${idEducacion}`);
+  }
+
+}
