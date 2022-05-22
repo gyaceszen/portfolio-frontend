17dfe3a73a94b7b339cbfa2da554c723128fad24
Gaston Yaceszen <gaston.vy@gmail.com>
2022-05-22 15:17:53 -0300

    funcionando boton agrergar y editar, seguir con delete

--- portfolioAngular/src/app/misComponentes/educacion/educacion.component.ts
+++ portfolioAngular/src/app/misComponentes/educacion/educacion.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Educacion } from 'src/app/models/educacion';
 import { EducacionService } from 'src/app/servicios/educacion.service';
 
@@ -11,6 +12,8 @@ import { EducacionService } from 'src/app/servicios/educacion.service';
 export class EducacionComponent implements OnInit {
 
   public educaciones: Educacion[]=[];
+  public editEducaciones: Educacion | undefined;
+  public borrarEducaciones: Educacion | undefined;
 
   constructor(private educacionService:EducacionService) { }
 
@@ -24,6 +27,66 @@ export class EducacionComponent implements OnInit {
         this.educaciones=Response;
       },
       error:(error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+  public onOpenModal(mode:String, educacion?: Educacion):void{
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.style.display='none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    if(mode==='add'){
+      button.setAttribute('data-bs-target', '#agregarEducacionModal');
+    }else if(mode==='delete'){
+      this.borrarEducaciones = educacion;
+      button.setAttribute('data-bs-target', '#deleteEducacionModal' )
+    }else if(mode==='edit'){
+      this.editEducaciones = educacion;
+      button.setAttribute('data-bs-target', '#editarEducacionModal' )
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onAddEducacion(addForm: NgForm){
+    document.getElementById('add-education-form')?.click();
+    this.educacionService.agregarEducacion(addForm.value).subscribe({
+      next: (response: Educacion) => {
+        console.log(response);
+        this.getEducaciones();
+        addForm.resetForm();
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message);
+        addForm.reset();
+      }
+    })
+  }
+
+  public onUpdateEducacion(educacion: Educacion){
+    this.editEducaciones=educacion;
+    document.getElementById('add-education-form')?.click();
+    this.educacionService.editarEducacion(educacion).subscribe({
+      next: (response: Educacion) => {
+        console.log(response);
+        this.getEducaciones();
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+  public onDeleteEducacion(idEducacion: number):void{
+    this.educacionService.eliminarEducacion(idEducacion).subscribe({
+      next: (response: void) => {
+        console.log(response);
+        this.getEducaciones();
+      },
+      error:(error:HttpErrorResponse)=>{
         alert(error.message);
       }
     })
