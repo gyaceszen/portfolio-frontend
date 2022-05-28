15638ed78466f4ba4c228841bb1ae72db9da0f8a
Gaston Yaceszen <gaston.vy@gmail.com>
2022-05-28 11:36:29 -0300

    agrego el editar usuario

--- portfolioAngular/src/app/misComponentes/acerca-de/acerca-de.component.ts
+++ portfolioAngular/src/app/misComponentes/acerca-de/acerca-de.component.ts
@@ -12,6 +12,7 @@ export class AcercaDeComponent implements OnInit {
 
   public usuario: Usuario | undefined;
   public editarUsuario: Usuario | undefined;
+  public borrarUsuario: Usuario | undefined;
   constructor(private acercadeService: AcercaDeService) { }
 
   ngOnInit(): void {
@@ -22,6 +23,40 @@ export class AcercaDeComponent implements OnInit {
     this.acercadeService.getUsuario().subscribe({
       next:(response: Usuario) => {
         this.usuario=response;
+      },
+      error:(error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+  public onOpenModal(mode:String, acercaDeUsuario?: Usuario):void{
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.style.display='none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    if(mode==='add'){
+      // button.setAttribute('data-bs-target', '#agregarUsuarioModal');
+    }else if(mode==='delete'){
+      this.borrarUsuario = acercaDeUsuario;
+      // button.setAttribute('data-bs-target', '#deleteUsuarioModal' )
+    }else if(mode==='edit'){
+      this.editarUsuario = acercaDeUsuario;
+      button.setAttribute('data-bs-target', '#editarUsuarioModal' )
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
+
+  //creo metodo para unicamente editar mi usuario
+  public onUpdateUsuario(acercaDeUsuario: Usuario){
+    this.editarUsuario=acercaDeUsuario;
+    document.getElementById('add-usuario-form')?.click();
+    this.acercadeService.editarUsuario(acercaDeUsuario).subscribe({
+      next: (response: Usuario) => {
+        console.log(response);
+        this.getUsuario();
       },
       error:(error:HttpErrorResponse)=>{
         alert(error.message);
